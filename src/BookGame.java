e5804811353100a079ed0484c5895217e6be5f67
Julio Cesar <juliocesar.starwars2004@gmail.com>
2023-06-15 20:11:45 -0300

    First line of story written

---
+++ src/BookGame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BookGame {
+    public static void main(String[] args) {
+        Scanner escaneador = new Scanner(System.in);
+        System.out.println("You wake up sitting in a hard wood table, in\n" 
+         + "a dimly lit room. As you barely make out the rest of the room,\n" 
+         + "you hear a faint voice, coming from across the same table you are sitting in."
+         + "\n\n'Grab the knife.'"
+         + "\n\nIt was unmistakably a very old man's voice. You could almost see a pair of wrinkled\n"
+         + "hands, and a large knife on the center of the table. It looked old, but had traces of blood.\n"
+         + "\nYour body trembles in fear, and you desperatly search for a way out.\n"
+         + "You're able to make out what seems to be a window in the distance.\n"
+         + "It looks closed, but fragile.\n"
+         + "\n\nType 'grab the knife' to grab it or 'run for to window' to try and escape.");
+    }
+}
